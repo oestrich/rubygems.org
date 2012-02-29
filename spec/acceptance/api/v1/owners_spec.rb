0219d292f1d4c99a8ec2c8e1a8b3c89ca8c2c812
Eric Oestrich <eric@oestrich.org>
2012-02-28 19:31:20 -0500

    Owners docs

--- spec/acceptance/api/v1/owners_spec.rb
+++ spec/acceptance/api/v1/owners_spec.rb
@@ -20,16 +20,12 @@ resource "Owners" do
     context "json" do
       let(:accept) { "application/json" }
 
-      example "Getting owners of a gem - JSON" do
-        do_request
-
+      example_request "Getting owners of a gem - JSON" do
         response_body.should == rubygem.owners.to_json
         status.should == 200
       end
 
-      example "Getting owners of a gem - Missing gem" do
-        do_request(:id => "missing")
-
+      example_request "Getting owners of a gem - Missing gem", :id => "missing" do
         response_body.should == "This rubygem could not be found."
         status.should == 404
       end
@@ -38,9 +34,7 @@ resource "Owners" do
     context "xml" do
       let(:accept) { "application/xml" }
 
-      example "Getting owners of a gem - XML" do
-        do_request
-
+      example_request "Getting owners of a gem - XML" do
         response_body.should == rubygem.owners.to_xml
         status.should == 200
       end
@@ -49,9 +43,7 @@ resource "Owners" do
     context "yaml" do
       let(:accept) { "text/yaml" }
 
-      example "Getting owners of a gem - YAML" do
-        do_request
-
+      example_request "Getting owners of a gem - YAML" do
         response_body.should == rubygem.owners.to_yaml
         status.should == 200
       end
@@ -68,18 +60,26 @@ resource "Owners" do
 
     let(:email) { user2.email }
 
-    example "Creating a new owner" do
-      do_request
-
+    example_request "Creating a new owner" do
       response_body.should == "Owner added successfully."
       status.should == 200
     end
 
-    example "Creating a new owner - Owner not found" do
-      do_request(:email => "not@found.com")
-
+    example_request "Creating a new owner - Owner not found", :email => "not@found.com" do
       response_body.should == "Owner could not be found."
       status.should == 404
+    end
+
+    context "no permission" do
+      before do
+        rubygem.ownerships.create(:user => Factory(:user))
+        rubygem.ownerships.find_by_user_id(user.id).destroy
+      end
+
+      example_request "Creating a new owner - No permission" do
+        response_body.should == "You do not have permission to manage this gem."
+        status.should == 401
+      end
     end
   end
 
@@ -101,18 +101,62 @@ resource "Owners" do
       status.should == 200
     end
 
-    example "Deleting an owner - Last one" do
-      do_request
-
+    example_request "Deleting an owner - Last one" do
       response_body.should == "Unable to remove owner."
       status.should == 403
     end
 
-    example "Deleting an owner - Owner not found" do
-      do_request(:email => "not@found.com")
-
+    example_request "Deleting an owner - Owner not found", :email => "not@found.com" do
       response_body.should == "Owner could not be found."
       status.should == 404
+    end
+
+    context "no permission" do
+      before do
+        rubygem.ownerships.create(:user => Factory(:user))
+        rubygem.ownerships.find_by_user_id(user.id).destroy
+      end
+
+      example_request "Deleting an owner - No permission" do
+        response_body.should == "You do not have permission to manage this gem."
+        status.should == 401
+      end
+    end
+  end
+
+  get "/api/v1/owners/:handle/gems" do
+    let(:handle) { user.handle }
+
+    context "json" do
+      let(:accept) { "application/json" }
+
+      example_request "Getting a user's gems - JSON" do
+        response_body.should == user.rubygems.with_versions.to_json
+        status.should == 200
+      end
+
+      example_request "Getting a user's gems - Unknown user", :handle => "notfound" do
+        response_body.should == "Owner could not be found."
+        status.should == 404
+      end
+    end
+
+    context "xml" do
+      let(:accept) { "application/xml" }
+
+      example_request "Getting a user's gems - XML" do
+        response_body.should == user.rubygems.with_versions.to_xml
+        status.should == 200
+      end
+    end
+
+    context "yaml" do
+      let(:accept) { "text/yaml" }
+
+      example_request "Getting a user's gems - YAML" do
+        response_body.should == user.rubygems.with_versions.to_yaml
+        status.should == 200
+      end
     end
   end
 end
